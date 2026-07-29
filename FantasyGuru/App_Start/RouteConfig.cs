a51ffbf734f5f23f2da19454f3bf3a9d0069eceb
Bojan Kozoloski <bojan.kozoloski@yahoo.com>
2026-07-29 02:49:55 +0200

    Got rid of home controller and views of it

--- FantasyGuru/App_Start/RouteConfig.cs
+++ FantasyGuru/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace FantasyGuru
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Manager", action = "Guru", id = UrlParameter.Optional }
             );
         }
     }
